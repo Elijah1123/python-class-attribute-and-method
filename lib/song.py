2235fe6ff1fa5e138d04298ffd325b224834aa63
Elijah1123 <elijahmzalendo659@gmail.com>
2025-08-28 14:26:50 +0300

    python

---
+++ lib/song.py
@@ -0,0 +1,61 @@
+class Song:
+    
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+
+if __name__ == "__main__":
+    s1 = Song("Shape of You", "Ed Sheeran", "Pop")
+    s2 = Song("Blinding Lights", "The Weeknd", "R&B")
+    s3 = Song("Perfect", "Ed Sheeran", "Pop")
+
+    print(s1.name, "-", s1.artist, "-", s1.genre)
+    print(s2.name, "-", s2.artist, "-", s2.genre)
+
+    print("\nTotal songs:", Song.count)
+    print("Genres:", Song.genres)
+    print("Artists:", Song.artists)
+    print("Genre count:", Song.genre_count)
+    print("Artist count:", Song.artist_count)
